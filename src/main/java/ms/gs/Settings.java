c1c2f01e68804d0e2a1afb991e8f9b0ca2566c0c
Marc <marc.schoenig@gmail.com>
2022-07-09 19:48:05 +0200

    added new constant

--- src/main/java/ms/gs/Settings.java
+++ src/main/java/ms/gs/Settings.java
@@ -1,9 +1,10 @@
 package ms.gs;
 
 public class Settings {
+
     public static final float GRAVITY = 0.0019f;
     public static final float BOOST_VELOCITY = 0.028f;
-    public static final float FLOOR_VEOLOCITY = 0.2f;
-    public static final float BACKGROUND_VEOLOCITY = 0.07f;
+    public static final float FLOOR_VELOCITY = 0.2f;
+    public static final float BACKGROUND_VELOCITY = 0.07f;
     //TODO: Skins
 }
