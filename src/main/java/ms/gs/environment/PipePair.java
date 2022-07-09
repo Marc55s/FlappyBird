13560301cccf808d8c81b01179c9067727cc7560
Marc <marc.schoenig@gmail.com>
2022-07-09 19:54:57 +0200

    new constant for the gap

--- src/main/java/ms/gs/environment/PipePair.java
+++ src/main/java/ms/gs/environment/PipePair.java
@@ -8,15 +8,18 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class PipePair extends GameObject {
 
+    public static final int GAP = 160;
     private BufferedImage down;
     private BufferedImage up;
-    private final int heightDiff = 160;
+    private int offsetY = 60;
 
     public PipePair(String name, float speed, int x, int y, int width, int height) {
         super(name, speed, x, y, width, height);
+        setY(ThreadLocalRandom.current().nextInt(offsetY - getHeight(), -140));
         try {
             down = ImageIO.read(new File("src\\main\\resources\\Pipe\\pipe.png"));
             up = ImageIO.read(new File("src\\main\\resources\\Pipe\\pipeReversed.png"));
@@ -27,13 +30,22 @@ public class PipePair extends GameObject {
 
     @Override
     public void update(long elapsedTime) {
-        setX((int) (getX()-getSpeed()*elapsedTime));
-        if(getX() <= 0) setX(Main.WIDTH);
+        if (getX() <= 0)
+            setX((int) Math.floor(getX() - (getSpeed() * elapsedTime)));
+
+        setX((int) (getX() - (getSpeed() * elapsedTime)));
+
+        if (getX() <= -getWidth()) {
+            setX(Main.WIDTH + getWidth());
+            setY(ThreadLocalRandom.current().nextInt(offsetY - getHeight(), -140));
+        }
     }
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(down,getX(),getY(),getWidth(),getHeight(),null);
-        g.drawImage(up,getX(),getY()+getHeight()+heightDiff,getWidth(),getHeight(),null);
+        g.drawImage(down, getX(), getY(), getWidth(), getHeight(), null);
+        g.drawImage(up, getX(), getY() + getHeight() + GAP, getWidth(), getHeight(), null);
+
     }
+
 }
