b8cb4943092fa69e68aff12e97623d19c3c6c5bb
Marc <marc.schoenig@gmail.com>
2022-07-10 20:49:25 +0200

    menu class and images

---
+++ src/main/java/ms/gs/Menu.java
@@ -0,0 +1,35 @@
+package ms.gs;
+
+import javax.imageio.ImageIO;
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Menu extends GameObject{
+
+    private BufferedImage img;
+    public Menu(String name, float speed, int x, int y, int width, int height) {
+        super(name, speed, x, y, width, height);
+        try {
+            img = ImageIO.read(new File("src\\main\\resources\\Background\\startscreentransparent.png"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.setColor(new Color(.1f,.1f,.3f,0.6f));
+        g.fillRect(0,0,Main.WIDTH,Main.HEIGHT);
+        g.drawImage(img,getX(),getY(),getWidth(),getHeight(),null);
+    }
+
+    @Override
+    public void update(long elapsedTime) {
+
+    }
+}
