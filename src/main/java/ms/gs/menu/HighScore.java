273d9bd41a57612063e6dc77ff9eba083716f709
Marc <marc.schoenig@gmail.com>
2022-08-13 02:14:23 +0200

    Highscore creates file if non-existent

--- src/main/java/ms/gs/menu/HighScore.java
+++ src/main/java/ms/gs/menu/HighScore.java
@@ -10,6 +10,7 @@ import java.awt.Graphics;
 import java.io.*;
 
 public class HighScore extends GameObject implements Serializable {
+
     @Serial
     private static final long serialVersionUID = -7629368922699364878L;
     private Font f = null;
@@ -17,6 +18,8 @@ public class HighScore extends GameObject implements Serializable {
     private double score = 0;
     private int highScore = 0;
     File dir;
+    File serializable;
+
 
 
     public HighScore(String name, float speed, int x, int y, int width, int height) {
@@ -26,41 +29,37 @@ public class HighScore extends GameObject implements Serializable {
             f = Font.createFont(Font.TRUETYPE_FONT, new File("src/main/resources/Font/04B_19__.TTF")).deriveFont(80f);
             f2 = Font.createFont(Font.TRUETYPE_FONT, new File("src/main/resources/Font/04B_19__.TTF")).deriveFont(60f);
             dir = new File("src\\main\\resources\\save");
-            serilizable = new File("src\\main\\resources\\save\\highscore.ser");
-        } catch (FontFormatException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
+            serializable = new File("src\\main\\resources\\save\\highscore.ser");
+        } catch (FontFormatException | IOException e) {
             throw new RuntimeException(e);
         }
-        if (serilizable.exists())
+        if (serializable.exists()) {
             this.highScore = getBestScoreFromFile();
-    }
+        }
 
-    File serilizable;
+    }
 
     private void saveBestScore() {
         // TODO: 15.07.2022 save in temp
-        FileOutputStream fos = null;
-        ObjectOutputStream oos = null;
-        try {
-            fos = new FileOutputStream(serilizable);
-            oos = new ObjectOutputStream(fos);
-            oos.writeObject(this);
-            oos.flush();
-            oos.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
         if (dir.mkdir()) {
             try {
-                if (serilizable.createNewFile()) {
-                    System.out.println("file created");
+                if (serializable.createNewFile()) {
+                    System.out.println("highscore file created");
                 }
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+        }
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        try {
+            fos = new FileOutputStream(serializable);
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(this);
+            oos.flush();
+            oos.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -68,7 +67,7 @@ public class HighScore extends GameObject implements Serializable {
         FileInputStream fis = null;
         HighScore fileObj;
         try {
-            fis = new FileInputStream(serilizable);
+            fis = new FileInputStream(serializable);
             ObjectInputStream ois = new ObjectInputStream(fis);
             fileObj = (HighScore) ois.readObject();
             ois.close();
@@ -92,7 +91,7 @@ public class HighScore extends GameObject implements Serializable {
         String s = String.valueOf((int) score);
         g.setFont(f);
         g.drawString(s, getX(), getY());
-        String bestScore = "Best: " + String.valueOf(this.highScore);
+        String bestScore = "Best: " + this.highScore;
         if (Main.gameState.equals(GameState.MENU)) {
             g.setFont(f2);
             g.drawString(bestScore, Main.WIDTH / 2 - 100, Main.HEIGHT - 400);
